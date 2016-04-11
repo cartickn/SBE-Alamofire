0f304969e0ea4112b3fe3887d41177b5b8d49258
Karthik Murugesan <cartic@gmail.com>
2016-04-10 20:50:45 -0700

    Add SBE-Alamofire Project

---
+++ SBE-Alamofire/SBE-Alamofire/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  SBE-Alamofire
+//
+//  Created by Karthik Murugesan on 4/10/16.
+//  Copyright © 2016 Karthik Murugesan. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        Alamofire.request(.GET, "https://httpbin.org/ip", parameters: ["foo": "bar"])
+            .responseJSON { response in
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+                print(response.data)     // server data
+                print(response.result)   // result of response serialization
+                
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)")
+                }
+        }
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
